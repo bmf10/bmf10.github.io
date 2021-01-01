911b24e1f5dd7ab714363c15c5ec06b6720bb044
BmF10 <bimafebriansyah1002@gmail.com>
2021-01-01 10:45:25 +0700

    init tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./containers/Home";
 
-function App() {
+const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Route exact path="/" component={Home} />
+    </HashRouter>
   );
-}
-
+};
 export default App;
---
+++ src/containers/Home/index.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+const Home: FC = () => {
+  return (
+    <div className="h-screen bg-gray-800">
+      <div className="container mx-auto flex items-center justify-center h-full">
+        <h1 className="font-mono text-white text-2xl">Hello World</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
