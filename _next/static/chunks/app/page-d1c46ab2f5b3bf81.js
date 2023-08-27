d0512f952883d58177ac81706bcad725044d77eb
Bima Febriansyah <bimafebriansyah1002@gmail.com>
2023-08-27 18:25:05 +0700

    Updates

--- _next/static/chunks/app/page-d1c46ab2f5b3bf81.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{3090:function(n,e,u){Promise.resolve().then(u.t.bind(u,3222,23))}},function(n){n.O(0,[222,971,596,744],function(){return n(n.s=3090)}),_N_E=n.O()}]);
